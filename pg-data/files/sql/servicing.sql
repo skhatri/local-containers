4559678e1edf37df68b9f7c57d0e4801a3db79fa
skhatri <servlet2@msn.com>
2024-01-29 22:40:40 +1100

    add comments for postgres schema

--- pg-data/files/sql/servicing.sql
+++ pg-data/files/sql/servicing.sql
@@ -4,6 +4,14 @@ create database servicing;
 
 CREATE SCHEMA IF NOT EXISTS customer;
 
+COMMENT ON
+    SCHEMA customer IS '{
+    "discovery.ops.catalog/skip": "false",
+    "discovery.ops.catalog/team": "crm", 
+    "discovery.ops.catalog/domain": "servicing",
+    "discovery.ops.catalog/capability": "client experience",
+    "discovery.ops.catalog/includes": "internal"
+  }';
 
 
 
